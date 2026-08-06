01d58a2633bd5d5b8bf3982cca740d7d7e7658dc
david <david@yoctobyte.nl>
2026-08-06 13:33:32 +0200

    matter_ethernet_stub.cpp: add debug prints

--- components/matter/matter_ethernet_stub.cpp
+++ components/matter/matter_ethernet_stub.cpp
@@ -1,6 +1,7 @@
 #include "esphome/core/defines.h"
 #ifdef USE_MATTER
 
+// Includes MUST come before CHIP_DEVICE_CONFIG_ENABLE_ETHERNET
 #include <platform/internal/CHIPDeviceLayerInternal.h>
 #include <platform/ESP32/NetworkCommissioningDriver.h>
 #include <esp_netif.h>
@@ -17,19 +18,37 @@
 // Both functions are defined in the `chip` static library archive. The linker prefers definitions
 // from object files over archive members, so these strong-symbol definitions win without errors.
 
+#include "esphome/core/log.h"
+
+static const char *const TAG = "matter";
+
 namespace chip::DeviceLayer {
 
-CHIP_ERROR ConnectivityManagerImpl::InitEthernet() { return CHIP_NO_ERROR; }
-void ConnectivityManagerImpl::OnEthernetIPv4AddressAvailable(const ip_event_got_ip_t &) {}
-void ConnectivityManagerImpl::OnEthernetIPv4AddressLost() {}
-void ConnectivityManagerImpl::OnEthernetIPv6AddressAvailable(const ip_event_got_ip6_t &) {}
-void ConnectivityManagerImpl::OnEthernetPlatformEvent(const ChipDeviceEvent *) {}
+CHIP_ERROR ConnectivityManagerImpl::InitEthernet() {
+    ESP_LOGD(TAG, "InitEthernet called");
+    return CHIP_NO_ERROR;
+}
+void ConnectivityManagerImpl::OnEthernetIPv4AddressAvailable(const ip_event_got_ip_t &) {
+    ESP_LOGD(TAG, "OnEthernetIPv4AddressAvailable called");
+}
+void ConnectivityManagerImpl::OnEthernetIPv4AddressLost() {
+    ESP_LOGD(TAG, "OnEthernetIPv4AddressLost called");
+}
+void ConnectivityManagerImpl::OnEthernetIPv6AddressAvailable(const ip_event_got_ip6_t &) {
+    ESP_LOGD(TAG, "OnEthernetIPv6AddressAvailable called");
+}
+void ConnectivityManagerImpl::OnEthernetPlatformEvent(const ChipDeviceEvent *) {
+    ESP_LOGD(TAG, "OnEthernetPlatformEvent called");
+}
 
 } // namespace chip::DeviceLayer
 
 namespace chip::DeviceLayer::NetworkCommissioning {
 
-CHIP_ERROR ESPEthernetDriver::Init(NetworkStatusChangeCallback *) { return CHIP_NO_ERROR; }
+CHIP_ERROR ESPEthernetDriver::Init(NetworkStatusChangeCallback *) {
+    ESP_LOGD(TAG, "ESPEthernetDriver::Init called");
+    return CHIP_NO_ERROR;
+}
 
 } // namespace chip::DeviceLayer::NetworkCommissioning
 
