c40f56d90f847298670b29ea667b0a4823631dea
creepystefan <147045262+creepystefan@users.noreply.github.com>
2026-09-07 17:50:41 +0200

    Update endpoints.py

--- components/matter/endpoints.py
+++ components/matter/endpoints.py
@@ -1,6 +1,6 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.const import CONF_LIGHT_ID, CONF_SENSOR_ID
+from esphome.const import CONF_LIGHT_ID, CONF_SENSOR_ID, CONF_NAME
 from esphome.types import ConfigType
 
 from .const import *
