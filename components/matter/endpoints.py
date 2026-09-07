6c871f83673c5f56e22a629a4935388187bf5246
creepystefan <147045262+creepystefan@users.noreply.github.com>
2026-09-07 17:22:05 +0200

    Update endpoints.py

--- components/matter/endpoints.py
+++ components/matter/endpoints.py
@@ -13,6 +13,7 @@ ENDPOINT_SCHEMA = cv.All(
         {
             cv.GenerateID(): cv.declare_id(MatterEndpointRef),
             cv.Optional(CONF_ENABLE_BINDING): cv.boolean,
+            cv.Optional(CONF_NAME): cv.string_strict,
         }
         | {
             cv.Optional(dt_name): dt_config["schema"]
